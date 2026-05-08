90aecbe0be23d58260b29c5a2aab05de2e8af0ab
Simon Pieters <zcorpan@gmail.com>
2026-05-08 12:29:08 +0200

    Initial commit

---
+++ navigator-keyboard-lock-polyfill.js
@@ -0,0 +1,34 @@
+/*
+ * navigator.keyboard.lock polyfill
+ * Copyright (c) 2026 Simon Pieters
+ * Licensed under the MIT License.
+ */
+ (() => {
+  let locked = false;
+
+  const origRequestFullscreen = Element.prototype.requestFullscreen;
+
+  Element.prototype.requestFullscreen = function (options = {}) {
+    if (!("keyboardLock" in options)) {
+      options.keyboardLock = locked ? "browser" : "none";
+    }
+    return origRequestFullscreen.call(this, options);
+  };
+
+  navigator.keyboard = {
+    lock: () => {
+      const lockStateChanged = locked === false;
+      locked = true;
+      if (lockStateChanged && document.fullscreenElement && navigator.userActivation.isActive) {
+        document.fullscreenElement.requestFullscreen();
+      }
+    },
+    unlock: () => {
+      const lockStateChanged = locked === true;
+      locked = false;
+      if (lockStateChanged && document.fullscreenElement && navigator.userActivation.isActive) {
+        document.fullscreenElement.requestFullscreen();
+      }
+    },
+  };
+})();
